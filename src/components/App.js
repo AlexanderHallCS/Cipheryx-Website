eb5d3266b8b59f4f20562010dca0997ea61f44b7
AlexanderHallCS <hallale21@yahoo.com>
2023-03-22 00:14:18 -0500

    Delete unused files, restructure folders

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Home from "../pages/Home";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />}>
+          {/* <Route index element={<Home />} /> */}
+          {/* ...*/}
+        </Route>
+    </Routes>
+  );
+}
+
+export default App;
