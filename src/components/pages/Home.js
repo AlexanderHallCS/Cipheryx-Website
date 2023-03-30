190af6f59ae5b1c9f8240ec63e506132d456cd0b
AlexanderHallCS <hallale21@yahoo.com>
2023-03-30 02:33:19 -0500

    Add YT and Twitch links to Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react"
+import "../../styles/Home.css"
+
+const YTButton = require('../../images/YTButton.png')
+const TwitchButton = require('../../images/TwitchButton.png')
 
 export default function Home() {
     return (
-        <></>
+        <>
+            <h1 className="home-title">
+                CIPHERYX
+            </h1>
+            <h2 className="home-subtitle">
+            - VTuber and iOS Developer :)
+            </h2>
+            <a href="https://www.youtube.com/@cipheryx" target="_blank" rel="noreferrer">
+                <img className="yt-button" alt="youtube link" src={YTButton}/>
+            </a>
+            <a href="https://www.twitch.tv/cipheryx" target="_blank" rel="noreferrer">
+                <img className="twitch-button" alt="twitch link" src={TwitchButton}/>
+            </a>
+        </>
     )
 }
