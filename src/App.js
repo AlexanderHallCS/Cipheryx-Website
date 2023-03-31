89fe6a18eed5ae6215d00bcd5cb9b7bcce0c81af
AlexanderHallCS <hallale21@yahoo.com>
2023-03-31 02:57:22 -0500

    Add copyright attribution footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppPrivacy from "./components/pages/AppPrivacy";
 import FlappyOhioHome from "./components/pages/apps/FlappyOhioHome";
 import FlappyOhioPrivacy from "./components/pages/privacy/FlappyOhioPrivacy";
 import './styles/App.css';
+import Attribution from "./components/Attribution";
 
 const BG = require('./images/BG.jpg')
 const divBGStyle = {
@@ -19,14 +20,14 @@ function App() {
   return (
     <>
         <Navbar/>
-        
-            <Routes>
-                <Route path="/" element={<div style={divBGStyle}><Home /></div>}/>
-                <Route path="/apps" element={<div style={divBGStyle}><Apps /></div>}/>
-                <Route path="/app-privacy" element={<div style={divBGStyle}><AppPrivacy /></div>}/>
-                <Route path="/apps/flappy-ohio" element={<div style={divBGStyle}><FlappyOhioHome /></div>}/>
-                <Route path="/apps/flappy-ohio/privacy" element={<FlappyOhioPrivacy />}/>
-            </Routes>
+        <Routes>
+            <Route path="/" element={<div style={divBGStyle}><Home /></div>}/>
+            <Route path="/apps" element={<div style={divBGStyle}><Apps /></div>}/>
+            <Route path="/app-privacy" element={<div style={divBGStyle}><AppPrivacy /></div>}/>
+            <Route path="/apps/flappy-ohio" element={<div style={divBGStyle}><FlappyOhioHome /></div>}/>
+            <Route path="/apps/flappy-ohio/privacy" element={<FlappyOhioPrivacy />}/>
+        </Routes>
+        <Attribution />
     </>
   );
 }
---
+++ src/components/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Attribution.css"
+
+export default function Attribution() {
+    return  (
+        <footer class="footer">
+            <div class="footerContainer">
+                <p class="copyright">© 2023 Cipheryx</p>
+            </div>
+        </footer>
+    );
+}
